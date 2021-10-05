d6b05cae3487ebf09ed457fbc16529b0f73a73a7
ThansudaJ21 <thansuda_janthakham.cmu.ac.th>
2021-10-05 10:51:14 +0700

    Fix deployment6

--- src/main/java/se331/lab/rest/Lab07Application.java
+++ src/main/java/se331/lab/rest/Lab07Application.java
@@ -20,6 +20,7 @@ public class Lab07Application {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://34.238.136.193:8001")
                         .exposedHeaders("x-total-count");
             }
         };
